d12a676f72a716167cdee442e0d6338e9025495d
Michael Frangione <mfrangione@gmail.com>
2019-11-22 11:57:24 -0500

    Updates

---
+++ precache-manifest.355e7d9d332b06dd5208e14bcee932b7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9039fd358c0fc7c34f35340226cdc483",
+    "url": "/Hungr/index.html"
+  },
+  {
+    "revision": "54be3ed530608ac64e3b",
+    "url": "/Hungr/static/css/2.d674b600.chunk.css"
+  },
+  {
+    "revision": "85523749263a93c86996",
+    "url": "/Hungr/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "54be3ed530608ac64e3b",
+    "url": "/Hungr/static/js/2.2235343b.chunk.js"
+  },
+  {
+    "revision": "85523749263a93c86996",
+    "url": "/Hungr/static/js/main.9c00ae37.chunk.js"
+  },
+  {
+    "revision": "f9674f7f8db35142e55b",
+    "url": "/Hungr/static/js/runtime-main.a999d14e.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Hungr/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Hungr/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Hungr/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Hungr/static/media/slick.f97e3bbf.svg"
+  }
+]);
